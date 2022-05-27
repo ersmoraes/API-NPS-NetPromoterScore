05d7b13785009bef869603379d04327ba01b3ee1
Eric Moraes <ersmoraes@outlook.com>
2022-05-27 15:10:46 -0300

    Criação do servidor em Express e configurando o projeto

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  return res.json({
+    message: "Hellow World - NLW04"
+  });
+});
+
+app.post('/', (req, res) => {
+  return res.json({
+    message: "Os dados foram salvos com sucesso!"
+  })
+})
+
+app.listen(3333, () => console.log('Server is running!'));
