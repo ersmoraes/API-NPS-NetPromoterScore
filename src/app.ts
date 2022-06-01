37ba4d7731406e63b49216e3400ca5e61b4ce89e
Eric Moraes <ersmoraes@outlook.com>
2022-05-31 22:37:03 -0300

    adicionando testes de integrações em Jest

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata'
+import express from 'express';
+import { router } from './routes';
+import createConnections from "./database";
+
+createConnections();
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+export { app }
